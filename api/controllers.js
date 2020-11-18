8dd4fb89ca3c7d7152b8a571607d7c7fa97bb410
sayed94h <skazimi951@yahoo.com>
2020-11-18 14:10:51 +0100

    fixing deployment issue

--- api/controllers.js
+++ api/controllers.js
@@ -5,8 +5,8 @@ const path = require("path");
 const tv4 = require("tv4");
 const config = require("../config");
 
-const SCHEMA = path.join(__dirname, "/..", config.DATA_DIR, "/_-schema.json");
-const DATA_PATH = path.join(__dirname, "/..", config.DATA_DIR, "/_-data.json");
+const SCHEMA = path.join(__dirname, "/..", "/data", "/_-schema.json");
+const DATA_PATH = path.join(__dirname, "/..", "/data", "/_-data.json");
 
 const controllers = {
 	hello: (req, res) => {
